77c937c0d43caee3eeba663e80c27d7d84f6cdf5
tsehalff <tsehalffff@gmail.com>
2021-06-20 05:12:59 +0900

    initial commit

---
+++ component/HeaderItem.js
@@ -0,0 +1,12 @@
+
+
+function HeaderItem({ Icon, title }) {
+    return (
+        <div className="group flex flex-col items-center hover:text-white cursor-pointer w-12 sm:w-20 ">
+           <Icon className="h-8 mb-1 group-hover:animate-bounce" />
+           <p className="opacity-0 group-hover:opacity-100 tracking-widest" >{title}</p>     
+        </div>
+    )   
+}
+
+export default HeaderItem
